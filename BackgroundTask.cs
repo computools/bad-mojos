5de5b5aec4d435a7fdeb772fbebf8bc1f6181f1f
Oleg Svet <o@computools.com>
2019-09-30 18:01:38 +0300

    Add files via upload
    init

---
+++ BackgroundTask.cs
@@ -0,0 +1,33 @@
+﻿#if WSA_PLUGIN
+
+using Windows.ApplicationModel.Background;
+
+namespace ViewzNotifications.WSA
+{
+    public sealed class BackgroundTask : IBackgroundTask
+    {
+        public static void Register()
+        {
+            string taskName = typeof(BackgroundTask).FullName.ToLower();
+
+            if (NotificationTools.IsBackgroundTaskRegistered(taskName))
+            {
+                return;
+            }
+
+            BackgroundTaskBuilder builder = new BackgroundTaskBuilder();
+
+            builder.TaskEntryPoint = typeof(BackgroundTask).FullName;
+            builder.Name = taskName;
+            builder.SetTrigger(new MaintenanceTrigger(15, false));
+
+            builder.Register();
+        }
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            NotificationTools.Reschedule(false);
+        }
+    }  
+}
+#endif
